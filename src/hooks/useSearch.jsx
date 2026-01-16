1272ae837865e554ae62d2a75e34918aafe7aa9a
lhgeer7485 <lhgeer2617@gmail.com>
2026-01-16 17:06:39 +0900

    feat: 검색기능 디바운싱 적용

--- src/hooks/useSearch.jsx
+++ src/hooks/useSearch.jsx
@@ -1,11 +1,23 @@
 import ZustandStore from "../stores/store.jsx";
+import { useEffect, useState } from "react";
 
 const useSearch = () => {
   const setSearchKeyword = ZustandStore((state) => state.setSearchKeyword);
+  const [value, setValue] = useState("");
+
+  // 디바운싱
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setSearchKeyword(value);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, setSearchKeyword]);
 
   const onChange = (e) => {
-    const { value } = e.target;
-    setSearchKeyword(value);
+    setValue(e.target.value);
   };
 
   return { onChange };
